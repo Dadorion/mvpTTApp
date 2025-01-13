ffced9e02927dfef6c558179ade2d4bf5f871082
Anton Babenko <anton.notes88@gmail.com>
2025-01-13 17:29:00 +0400

    Создал страницу статистики призрака

--- client/src/pages/content/Content.jsx
+++ client/src/pages/content/Content.jsx
@@ -11,6 +11,7 @@ import Tournament from "../tournament/Tournament";
 import TournamentPlay from "../tournamentPlay/TournamentPlay";
 import TournamentSummary from "../TournamentSummary/TournamentSummary";
 import PlayersHome from "../players_home/PlayersHome";
+import PlayerStats from "../playerStats/PlayerStats"
 import Players from "../players/Players";
 import Congratulations from "../Congratulations/Congratulations";
 import TournamentStart from "pages/tournamentStart/TournamentStart";
@@ -20,6 +21,7 @@ function Content() {
     <div className={s.Content}>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/player:id" element={<PlayerStats />} />
         <Route path="/tournaments" element={<Tournament />} />
         <Route path="/preference" element={<Preference />} />
         <Route path="/profile" element={<Profile />} />
--- client/src/pages/playerStats/PlayerStats.jsx
+++ client/src/pages/playerStats/PlayerStats.jsx
@@ -2,20 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import s from "./HomePage.module.scss";
+import s from "./PlayerStats.module.scss";
 
 import {
   setLossesListTC,
   setStatsTC,
 } from "services/redux/reducers/home-reducer";
 
-import CustomButton from "components/CustomButtonBold/CustomButtonBold";
 import Preloader from "components/PreloaderMini/Preloader";
 
-function HomePage() {
+function PlayerStats() {
   const dispatch = useDispatch();
-
-  const [navToPlayers, setNavToPlayers] = useState(false);
 
   const [loadingState, setLoadingState] = useState({
     total: true,
@@ -41,10 +38,6 @@ function HomePage() {
       <td>{p.count}</td>
     </tr>
   ));
-
-  const handleGoToPlayers = () => {
-    setNavToPlayers(true);
-  };
 
   const simulateLoading = (key) => {
     setTimeout(() => {
@@ -82,40 +75,35 @@ function HomePage() {
   if (!isAuth) {
     return <Navigate to="/login" />;
   }
-  if (navToPlayers) {
-    return <Navigate to="/players-home" />;
-  }
 
   return (
-    <div className={s.HomePage}>
-      <h1>Мои победы</h1>
-      <h3>Показатели эффективности</h3>
-      <div className={s.wins_mainInfo}>
-        <div className={s.info}>
-          {renderStat(mainInfo.total, "total", "Total")}
-          {renderStat(mainInfo.wins, "wins", "Wins")}
-          {renderStat(mainInfo.matches, "matches", "Matches")}
-          {renderStat(mainInfo.tournaments, "tournaments", "Tournaments")}
-        </div>
-      </div>
-      <h3>Топ самых сложных игроков</h3>
-      <div className={s.wins_players}>
-        <table>
-          <thead>
-            <tr>
-              <th>Игрок</th>
-              <th>Поражений</th>
-            </tr>
-          </thead>
-          <tbody>{topList}</tbody>
-        </table>
-      </div>
-      <CustomButton
-        title={"Посмотреть всех игроков"}
-        onClick={handleGoToPlayers}
-      />
-    </div>
-  );
+   <div className={s.HomePage}>
+     <h1>Победы игрока</h1>
+     <h3>Показатели эффективности</h3>
+     <div className={s.wins_mainInfo}>
+       <div className={s.info}>
+         {renderStat(mainInfo.total, "total", "Total")}
+         {renderStat(mainInfo.wins, "wins", "Wins")}
+         {renderStat(mainInfo.matches, "matches", "Matches")}
+         {renderStat(mainInfo.tournaments, "tournaments", "Tournaments")}
+       </div>
+     </div>
+     <h3>Топ самых сложных игроков</h3>
+     <div className={s.wins_players}>
+       <table>
+         <thead>
+           <tr>
+             <th>Игрок</th>
+             <th>Поражений</th>
+           </tr>
+         </thead>
+         <tbody>{topList}</tbody>
+       </table>
+     </div>
+   </div>
+ );
 }
 
-export default HomePage;
+
+
+export default PlayerStats;
